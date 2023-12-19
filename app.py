352e27b4dfeb080f0adf412f502d75d541d92844
hamza <bouzianh78@gmail.com>
2023-12-19 10:36:42 +0100

    adding mult function

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
 def ident(name):
     print(f"my name is {name}")
 ident("hamza")
+def adding(num1,num2):
+    return num1+num2
+print(adding(23,4))
+def mult(num3,num4):
+    return num3*num4
+print(mult(2,4))
